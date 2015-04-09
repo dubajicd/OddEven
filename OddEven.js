7cbe6851ebe3243e50f8e4192ac4aa6b44510f5b
dubajicd <dubajicd@gmail.com>
2015-04-09 11:16:10 +0200

    Update OddEven.js

--- OddEven.js
+++ OddEven.js
@@ -3,14 +3,7 @@
  * Date: 12.02.2015
  * @param selector
  */
-function centerImages(selector){
-    jQuery(selector).each(function(){
-        var selectorHeight = jQuery( this).height();
-        var imageHeight = jQuery('img', this).height();
-        var calculateOffset = selectorHeight - imageHeight;
-        if(calculateOffset > 0){
-            var calculatedTopMargin = calculateOffset/2;
-            jQuery('img', this).attr('style', 'margin-top:'+calculatedTopMargin+'px');
-        }
-    });
+function oddEven(selector){
+    jQuery("selector:odd").addClass("odd");
+    jQuery("selector:even").addClass("even");
 }
